646ffab361d19cb60969d40c6e85e1cc12e198bd
Joe Crescenzi <CouponPages@gmail.com>
2017-09-04 12:01:50 -0400

    Initial Commit

---
+++ TalkBoy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TalkBoy
+//
+//  Created by Joe Crescenzi on 9/4/17.
+//  Copyright © 2017 Joe Crescenzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
